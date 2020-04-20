6b3a651d947a57037ae31352ef2893b99370c9b5
jesun <jesunm@gmail.com>
2020-04-19 19:41:41 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gopkg.in/jesun/somearbitrarypackage.v0
+
+go 1.13
---
+++ somearbitrarypackage.go
@@ -0,0 +1,10 @@
+package somearbitrarypackage
+
+import (
+	"fmt"
+)
+
+// SomeArbitraryFunction demonstrates the importing of code from github
+func SomeArbitraryFunction() {
+	fmt.Println("Inside SomeArbitraryFunction()...")
+}
